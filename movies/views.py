da6cbdca8876a0bcd95021de3971e17aecbd5670
mayowa-ola <mayowaoladunjoye@gmail.com>
2020-10-08 00:35:56 +0100

    vidly app setup and deployment

---
+++ movies/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+# Create your views here.
+
+def index(request):
+    movies = Movie.objects.all()
+    # output = ', '.join([m.title for m in movies])
+    # Movie.objects.filter(release_year=1965)
+    # Movie.objects.get(id=1)
+    # return HttpResponse(output)
+
+    return render(request, 'movies/index.html', {'movies':movies})
+
+def detail(request, movie_id):
+    # try: 
+    #     movie = Movie.objects.get(id=movie_id)
+    #     return render(request, 'movies/detail.html', {'movie':movie})
+    # except Movie.DoesNotExist:
+    #     raise Http404()
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/detail.html', {'movie':movie})
+
